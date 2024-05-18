d0a698d7c4ad4ec4c86cea064c4d0ea1b7064592
Nabeel Shakeel <nabeel.shakeel@techwards.co>
2024-05-18 13:50:27 +0500

    feat: add logic to parse command line args, fetch and show todos

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,63 @@
-function todosCLI(name: string) {
-  console.log(`Todos CLI: ${name}`);
+import { Command } from 'commander';
+import { table } from 'table';
+import { red, cyan, magenta } from 'kolorist';
+
+interface ITodo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
 }
+
+const program = new Command();
+
+program
+  .name('td')
+  .description('A utility to track your todos from the command line')
+  .version('1.0.0');
+
+// available options
+program
+  .option('-n, --number <number>', 'number of todos', '20')
+  .option('-e, --even', 'fetch even indexed todos')
+  .option('--no-even', 'fetch odd indexed todos')
+
+// process command line args from user
+program.parse(process.argv);
+const options = program.opts();
+
+const fetchTodos = async (number: number, even: boolean) => {
+  const promises = [];
+  for (let i = 1; i <= number; i++) {
+    if ((even && i % 2 === 0) || (!even && i % 2 !== 0)) {
+      promises.push(fetch(`https://jsonplaceholder.typicode.com/todos/${i}`));
+    }
+  }
+  const results = await Promise.all(promises);
+  return await Promise.all(results.map((response) => response.json()));
+};
+
+const displayTodos = (todos: ITodo[]) => {
+  // header for table
+  const data = [[magenta('Id'), magenta('Title'), magenta('Completed')]];
+
+  todos.forEach((todo) => {
+    data.push([String(todo.id), todo.title, String(todo.completed)]);
+  });
+
+  console.log(table(data));
+};
+
+async function init() {
+  try {
+    console.log(cyan('\nStart Fetching todos...\n'));
+    // By default, fetch even todos
+    const todos = await fetchTodos(parseInt(options.number), options.even ?? true);
+    displayTodos(todos);
+  } catch (error) {
+    console.error(red(`✖ Couldn't start the process. Error: ${error}`));
+  }
+}
+
+// calling the main function
+init();
